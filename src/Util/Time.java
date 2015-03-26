c18d6bda0ac264410d3b429fd7c8d04b4c59e100
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-03-25 23:42:52 -0400

    Comments to time class

--- src/Util/Time.java
+++ src/Util/Time.java
@@ -1,25 +1,45 @@
 package Util;
 
+/**
+ * All methods that deal with time will be made here. Time is a useful tool if you want the game to play with the same no matter how
+ * fast the game renders
+ * @author Ryan
+ *
+ */
 public class Time {
 	private float lastTime = System.currentTimeMillis();
 	private float deltaTime;
 	
-	
+	/**
+	 * This method should be called after every frame for keep all information up to date
+	 */
 	public void updateTime() {
 		float currentTime = System.currentTimeMillis();
 		deltaTime = currentTime - lastTime;
 		lastTime = currentTime;
 	}
 	
-	
+	/**
+	 * Returns the current time of the system clock. May possibly be useful if the game is has some
+	 * componenet that relies on real time.
+	 * @return
+	 */
 	public float getCuttentTime() {
 		return System.currentTimeMillis();
 	}
 	
+	/**
+	 * Returns the time from the last frame that was updated
+	 * @return
+	 */
 	public float getLastUpdateTime() {
 		return lastTime;
 	}
 	
+	/**
+	 * Returns the time when the last frame first began to be rendered and the time that the current frame is now being drawn
+	 * @return
+	 */
 	public float deltaTime() {
 		return deltaTime;
 	}
